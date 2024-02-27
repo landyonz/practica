f9818e23e43e4f7bdeb9375478dfefbc2545b125
landyonz <landy.onz@gmail.com>
2024-02-27 10:13:27 -0600

    cambios en 1

---
+++ git/nombres.java
@@ -0,0 +1,12 @@
+package git;
+
+import java.util.Scanner;
+public class nombres {
+
+	public static void main(String[] args) {
+		try (
+		Scanner sc = new Scanner(System.in)) {
+			System.out.println("Landy");
+		}
+	}
+}
